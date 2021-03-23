029753cb928ebfcac64a1b26806b1f4cca2be551
vikasvk11 <74751412+vikasvk11@users.noreply.github.com>
2021-03-23 09:30:12 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var header = document.getElementById("mainlink");
+var btns = header.getElementsByClassName("links");
+
+var copyBtn = document.querySelectorAll('.copy');
+
+var toast = document.querySelector('.copy-toast');
+
+for (var i=0; i<btns.length; i++) {
+
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        if(current.length>0){
+            current[0].className = current[0].className.replace(" active", "");
+        }
+        this.className += " active";
+    });
+}
+
+function copyText(str) {
+    console.log(str);
+    let code = document.querySelectorAll(`.${str}`);
+    let text = code[1].innerText;
+    navigator.clipboard.writeText(text)
+    .then(() => {
+            toast.classList.add('copy-toast-visible');
+            setTimeout(() => {
+                toast.classList.remove('copy-toast-visible');
+            },2500); 
+    });
+}
+
+Array.prototype.map.call(copyBtn, item => item.addEventListener('click', () => copyText(item.classList[2])));
